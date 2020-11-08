1eec6e43483ecaf57690ce408f0d87d33ec1a5e4
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2020-11-08 11:56:44 -0500

    Added dymaically laoded commands

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
+///////////////////////////////////////////////////////
+// Simple discord bot that will have some simple tools 
+// to help manage a discord community
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require("./auth.json");
-const config = require("./config.json");
 
+// Read all commands from the commands directory
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+// See README for how to setup your auth.json file with the correct token
+const auth = require("./auth.json");
+
+// Config file is where you can put your bot-specic settings in, like what prefix to use
+const config = require("./config.json");
+const prefix = config.prefix;
+
+// Logging message once the bot is logged in
 client.once('ready', () => {
 	console.log('Ready!');
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// For more info on how to set up dynamic commands, see 
+// the Discord.js docs: https://discordjs.guide/command-handling/dynamic-commands.html#how-it-works
 client.on('message', message => {
-	console.log(message.content);
+	// Ignore messages without the prefix or messages from this bot
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 });
 
